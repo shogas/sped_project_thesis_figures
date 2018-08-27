c66ed2c192fbbac0121c5f56785575558c0e1a8b
Simon Høgås <simhog@hotmail.no>
2018-08-27 14:43:52 +0200

    Generate 2D dataset

--- compare.py
+++ compare.py
@@ -9,39 +9,47 @@ import matplotlib.pyplot as plt
 
 from parameters import parseParameters, saveParameters
 
-def linearBlend(source_a, source_b, sample_count):
-    """ Blend of two sources, where the first third is entirely source a, the
-        last third entirely source b and the middle is a linear blend.
+def linearBlend(source_a, source_b, sample_width, sample_height):
+    """ Linear blend diffraction pattern sample
+
+        Blend of two sources in width direction, where the first third is
+        entirely source a, the last third entirely source b and the middle is a
+        linear blend. All samples in height direction are equal.
 
         source_a: Array of values representing one source
         source_b: Array of values representing the other source
-        sample_count: Number of samples to return
+        sample_width:  Number of samples wide
+        sample_height: Number of samples high
 
-        Result: Generator yielding the specified values.
+        Result: Generator yielding the specified values, iterating along width,
+        then height.
     """
 
     one_third = sample_count // 3
-    for i in range(one_third):
-        yield source_a
-    for i in range(1, one_third + 1):
-        t = i / one_third
-        yield (1-t)*source_a + t*source_b
-    for i in range(sample_count - 2*one_third):
-        yield source_b
+    for y in range(sample_height):
+        for i in range(one_third):
+            yield source_a
+        for i in range(1, one_third + 1):
+            t = i / one_third
+            yield (1-t)*source_a + t*source_b
+        for i in range(sample_count - 2*one_third):
+            yield source_b
 
 def noiselessRun(parameters, factorizer):
     source_a = matplotimg.imread(parameters['source_a_file'])
     source_b = matplotimg.imread(parameters['source_b_file'])
-    diffraction_patterns = linearBlend(source_a, source_b, int(parameters['sample_count']))
+    diffraction_patterns = linearBlend(
+            source_a, source_b,
+            int(parameters['sample_count_width']), int(parameters['sample_count_height']))
     return factorizer(diffraction_patterns)
 
 def debugFactorizer(diffraction_patterns):
     index = 4
     for i in range(index):
         next(diffraction_patterns)
-    asdf = next(diffraction_patterns)
-    print(asdf.shape)
-    plt.imshow(asdf, cmap='gray')
+    test_pattern = next(diffraction_patterns)
+    print(test_pattern.shape)
+    plt.imshow(test_pattern, cmap='gray')
     plt.show()
 
 def main(parameter_file):
