2450f7dcf7058c71dd32b3f4fbb9619f292f2646
Simon Høgås <simhog@hotmail.no>
2018-11-24 10:31:07 +0100

    Update combining of loadings after move, try fft before norm

--- combined_loading_map.py
+++ combined_loading_map.py
@@ -8,30 +8,44 @@ from tqdm import tqdm as report_progress
 
 import matplotlib
 matplotlib.use('Qt5Agg')
+import matplotlib.pyplot as plt
+
 import pyxem as pxm
 from pyxem.generators.indexation_generator import IndexationGenerator
+from pyxem.generators.structure_library_generator import StructureLibraryGenerator
 
 from common import result_image_file_info
 from parameters import parameters_parse
-from utils.template_matching import generate_diffraction_library, get_orientation_map
 
 def image_l2_norm(image_a, image_b):
+    image_a = np.fft.fft2(image_a)
+    image_a *= 1/image_a.max()
+    image_b = np.fft.fft2(image_b)
+    image_b *= 1/image_b.max()
     return np.linalg.norm(image_a - image_b, ord='fro')
 
 
-def classify_l2_norm(factor, known_factors):
-    threshold = 2
+def classify_l2_norm(parameters, factor, known_factors):
+    threshold = parameters['classify_l2_norm_threshold']
     if len(known_factors) > 0:
         diffs = [image_l2_norm(factor, known_factor) for known_factor in known_factors]
         best_diff_index = np.argmin(diffs)
         best_diff = diffs[best_diff_index]
         if (best_diff < threshold):
             factor_index = best_diff_index
-            report_progress.write('Matched phase {} (difference {})'.format(factor_index, best_diff))
+            # plt.figure()
+            # plt.suptitle(' '.join('{:.4f}'.format(d) for d in diffs))
+            # for i, (f, d) in enumerate(zip(known_factors, diffs)):
+                # plt.subplot(2, 4, i + 1)
+                # plt.imshow(f)
+            # plt.subplot(2, 4, 6)
+            # plt.imshow(factor)
+            # plt.show()
+            report_progress.write('    Matched phase {} (difference {})'.format(factor_index, best_diff))
         else:
             factor_index = len(known_factors)
             known_factors.append(factor)
-            report_progress.write('New phase {} (difference {})'.format(factor_index, best_diff))
+            report_progress.write('    New phase {} (difference {})'.format(factor_index, best_diff))
     else:
         factor_index = len(known_factors)
         known_factors.append(factor)
@@ -39,32 +53,66 @@ def classify_l2_norm(factor, known_factors):
     return factor_index
 
 
-def classify_template_match(factor, known_factors):
-    # factor = np.exp(factor)
+# TODO(simonhog): From compare/methods/template_match
+def create_diffraction_library(parameters, half_pattern_size):
+    specimen_thickness = parameters['specimen_thickness']
+    beam_energy_keV = parameters['beam_energy_keV']
+    reciprocal_angstrom_per_pixel = parameters['reciprocal_angstrom_per_pixel']
+    phase_names = [phase_name.strip() for phase_name in parameters['phase_names'].split(',')]
+    rotation_list_resolution = np.deg2rad(1)
+
+    phase_descriptions = []
+    inplane_rotations = []
+    for phase_name in phase_names:
+        structure = loadStructure(parameters['phase_{}_structure_file'.format(phase_name)])
+        crystal_system = parameters['phase_{}_crystal_system'.format(phase_name)]
+        rotations = [float(r.strip()) for r in str(parameters['phase_{}_inplane_rotations'.format(phase_name)]).split(',')]
+        phase_descriptions.append((phase_name, structure, crystal_system))
+        inplane_rotations.append([np.deg2rad(r) for r in rotations])
+
+    structure_library_generator = StructureLibraryGenerator(phase_descriptions)
+    structure_library = structure_library_generator.get_orientations_from_stereographic_triangle(
+            inplane_rotations, rotation_list_resolution)
+    max_excitation_error = 1/specimen_thickness
+    gen = DiffractionGenerator(beam_energy_keV, max_excitation_error=max_excitation_error)
+    library_generator = DiffractionLibraryGenerator(gen)
+    reciprocal_radius = reciprocal_angstrom_per_pixel*(half_pattern_size - 1)
+
+    diffraction_library = library_generator.get_diffraction_library(
+        structure_library,
+        calibration=reciprocal_angstrom_per_pixel,
+        reciprocal_radius=reciprocal_radius,
+        half_shape=(half_pattern_size, half_pattern_size),
+        with_direct_beam=False)
+
+    return diffraction_library
+
+
+diffraction_library = None
+def classify_template_match(parameters, factor, known_factors):
     dp = pxm.ElectronDiffraction([[factor]])
+    if diffraction_library is None:
+        diffraction_library = create_diffraction_library(parameters, dp.data.shape[2])
     pattern_indexer = IndexationGenerator(dp, diffraction_library)
     indexation_results = pattern_indexer.correlate(n_largest=4, keys=phase_names, show_progressbar=False)
     crystal_mapping = indexation_results.get_crystallographic_map(show_progressbar=False)
     phases = crystal_mapping.get_phase_map().data.ravel()
-    orientations = get_orientation_map(crystal_mapping).data.ravel()
+    orientations = crystal_mapping.get_orientation_map().data.ravel()
     for phase, orientation in zip(phases, orientations):
         if (phase, orientation) in known_factors:
             factor_index = known_factors.index((phase, orientation))
-            report_progress.write('Matched phase {}, {}'.format(phase, orientation))
+            report_progress.write('    Matched phase {}, {}'.format(phase, orientation))
         else:
             factor_index = len(known_factors)
             known_factors.append((phase, orientation))
-            report_progress.write('New phase {}, {}'.format(phase, orientation))
+            report_progress.write('    New phase {}, {}'.format(phase, orientation))
 
     return factor_index
 
 
-def combine_loading_map(method, factor_infos, loading_infos):
+def combine_loading_map(parameters, method, factor_infos, loading_infos, classify):
     total_width  = max((info['x_stop'] for info in loading_infos))
     total_height = max((info['y_stop'] for info in loading_infos))
-
-    classify = classify_l2_norm
-    # classify = classify_template_match
 
     combined_loadings = np.zeros((total_height, total_width, 3))
 
@@ -85,16 +133,19 @@ def combine_loading_map(method, factor_infos, loading_infos):
             ])
 
     known_factors = []
+    factor_info = factor_infos[0]
+    last_tile = (0, 0, 0, 0)
     for factor_info, loading_info in report_progress(zip(factor_infos, loading_infos), total=len(factor_infos)):
-        report_progress.write('Tile {}:{}  {}:{} (of {} {})'.format(
-            factor_info['x_start'], factor_info['x_stop'],
-            factor_info['y_start'], factor_info['y_stop'],
-            total_width, total_height))
+        tile = (factor_info['x_start'], factor_info['x_stop'],
+                factor_info['y_start'], factor_info['y_stop'])
+        if tile != last_tile:
+            report_progress.write('Tile {}:{}  {}:{} (of {} {})'.format(*tile, total_width, total_height))
+            last_tile = tile
         factor = np.asarray(Image.open(factor_info['filename']))
         factor = factor/factor.max()
 
-        factor_index = classify(factor, known_factors)
-        report_progress.write('Factor index: {}'.format(factor_index))
+        factor_index = classify(parameters, factor, known_factors)
+        report_progress.write('    Factor index: {}'.format(factor_index))
 
         loading = np.asarray(Image.open(loading_info['filename']))
         color = colors[factor_index % len(colors)]
@@ -106,21 +157,29 @@ def combine_loading_map(method, factor_infos, loading_infos):
     return Image.fromarray(combined_loadings.astype('uint8'))
 
 
-def combine_loading_maps(parametrs, result_directory):
-
+def combine_loading_maps(parameters, result_directory, classification_method):
     methods = [
             method.strip() for method in parameters['methods'].split(',')
             if parameters['__save_method_{}'.format(method.strip())] == 'decomposition']
     factor_infos = result_image_file_info(result_directory, 'factors')
     loading_infos = result_image_file_info(result_directory, 'loadings')
+    classify = {
+        'l2_norm': classify_l2_norm,
+        'template_match': classify_template_match,
+    }[classification_method]
     for (method_name, factor_infos_for_method), loading_infos_for_method in zip(factor_infos.items(), loading_infos.values()):
-        combined_loadings = combine_loading_map(method_name, factor_infos_for_method, loading_infos_for_method)
+        combined_loadings = combine_loading_map(
+                parameters,
+                method_name,
+                factor_infos_for_method,
+                loading_infos_for_method,
+                classify)
         combined_loadings.save(os.path.join(result_directory, 'loading_map_{}.tiff'.format(method_name)))
 
 if __name__ == '__main__':
     # TODO(simonhog): Make these less global. known_factors -> general dictionary for data?
     result_directory = sys.argv[1]
+    classification_method = sys.argv[2] if len(sys.argv) > 2 else 'l2_norm'
     parameters = parameters_parse(os.path.join(result_directory, 'metadata.txt'))
-    phase_names = ['ZB', 'WZ']
-    diffraction_library = generate_diffraction_library(parameters, phase_names)
-    combine_loading_maps(parameters, result_directory)
+    parameters['classify_l2_norm_threshold'] = float(sys.argv[3]) if len(sys.argv) > 3 else 5
+    combine_loading_maps(parameters, result_directory, classification_method)
--- do_all.bat
+++ do_all.bat
@@ -1,4 +1,5 @@
 @echo off
 
 python vdf.py runs\vdf.txt
+python combined_loading_map.py ..\..\Data\Runs\run_110_full_nmf_20181123_21_51_25_665830 l2_norm 3.2
 python figures.py runs\figures.txt
