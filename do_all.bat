e29e2790540370bc0f72fbd5d709521d6ed2cb33
Simon Høgås <simhog@hotmail.no>
2018-11-28 17:52:07 +0100

    Run file updates

--- do_all.bat
+++ do_all.bat
@@ -2,4 +2,7 @@
 
 python vdf.py runs\vdf.txt
 python combined_loading_map.py ..\..\Data\Runs\run_110_full_nmf_20181123_21_51_25_665830 l2_norm 3.2
+python combined_loading_map.py ..\..\Data\Runs\run_110_full_umap_a_20181126_23_17_20_750710 l2_norm_compare
+python combined_orientation.py ..\..\Data\Runs\run_110_full_template_match_20181123_09_18_24_085171
+REM matlab -nosplash -nodisplay -nodesktop -r "run test_ebsd.m"
 python figures.py runs\figures.txt
