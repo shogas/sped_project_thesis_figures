d4d0ae8bc0baf493d1c9fc82a479c3a109b8f3ad
Simon Høgås <simhog@hotmail.no>
2018-12-16 22:49:26 +0100

    Colorise phase map, colorbars for reliability

--- combined_orientation.py
+++ combined_orientation.py
@@ -16,7 +16,9 @@ from pyxem.signals.indexation_results import IndexationResults
 
 from common import result_object_file_info
 from parameters import parameters_parse
+from figure import material_color_palette
 from figure import save_figure
+from figure import TikzColorbar
 from figure import TikzImage
 from figure import TikzScalebar
 
@@ -59,47 +61,62 @@ def combine_orientations(result_directory):
         indexation_results = combine_indexation_results(object_infos['template_match'])
 
         crystal_map = indexation_results.get_crystallographic_map()
-        crystal_map.save_mtex_map(os.path.join(result_directory, 'mtex_test.csv'))
+        # crystal_map.save_mtex_map(os.path.join(result_directory, 'mtex_test.csv'))
         phase_map = crystal_map.get_phase_map()
         orientation_map = crystal_map.get_orientation_map()
         reliability_phase_map = crystal_map.get_reliability_map_phase()
         reliability_orientation_map = crystal_map.get_reliability_map_orientation()
         nav_width = phase_map.data.shape[1]
 
-        middle_slice = slice(int(0.25*nav_width), int(0.75*nav_width))
-        phase_map.data *= 255.0 / phase_map.data.max()
-        np.clip(phase_map.data, 0, 255.0, out=phase_map.data)
+        colored_phases = np.zeros((*phase_map.data.shape, 3))
+        colored_phases[phase_map.data == 0, :] = material_color_palette[0][1]  # Red ZB
+        colored_phases[phase_map.data == 1, :] = material_color_palette[2][1]  # Blue WZ
+        colored_phases *= 255.0
+        # middle_slice = slice(int(0.25*nav_width), int(0.75*nav_width))
         # reliability_orientation_map.data *= 255.0 / reliability_orientation_map.data[:, middle_slice].max()
         # reliability_phase_map.data *= 255.0 / reliability_phase_map.data[:, middle_slice].max()
+        rel_ori_min = reliability_orientation_map.data.min()
+        rel_ori_max = reliability_orientation_map.data.max()
+        rel_pha_min = reliability_phase_map.data.min()
+        rel_pha_max = reliability_phase_map.data.max()
+
         reliability_orientation_map.data *= 255.0 / 0.6
         reliability_phase_map.data *= 255.0 / reliability_phase_map.data.max()
 
         save_figure(
                 os.path.join(result_directory, 'phase_map.tex'),
-                TikzImage(phase_map.data.astype('uint8')),
+                TikzImage(colored_phases.astype('uint8')),
                 TikzScalebar(100, nav_scale_x*nav_width, r'\SI{100}{\nm}'))
         save_figure(
                 os.path.join(result_directory, 'reliability_orientation_map.tex'),
                 TikzImage(reliability_orientation_map.data.astype('uint8')),
-                TikzScalebar(100, nav_scale_x*nav_width, r'\SI{100}{\nm}'))
+                # TikzScalebar(100, nav_scale_x*nav_width, r'\SI{100}{\nm}'),
+                TikzColorbar(rel_ori_min, rel_ori_max, (rel_ori_max-rel_ori_min)/4,
+                    'blackwhite', r'0.8\textwidth',
+                    at=r'{(0.1\textwidth, 0)}',
+                    horizontal=True))
         save_figure(
                 os.path.join(result_directory, 'reliability_phase_map.tex'),
                 TikzImage(reliability_phase_map.data.astype('uint8')),
-                TikzScalebar(100, nav_scale_x*nav_width, r'\SI{100}{\nm}'))
+                # TikzScalebar(100, nav_scale_x*nav_width, r'\SI{100}{\nm}'))
+                TikzColorbar(rel_pha_min, rel_pha_max, (rel_pha_max-rel_pha_min)/4,
+                    'blackwhite', r'0.8\textwidth',
+                    at=r'{(0.1\textwidth, 0)}',
+                    horizontal=True))
 
-        save_figure(
-                os.path.join(result_directory, 'orientation_map_zb.tex'),
-                TikzImage('orientation_map_zb.png'))
-        save_figure(
-                os.path.join(result_directory, 'orientation_map_color_zb.tex'),
-                TikzImage('orientation_map_color_zb.pdf'))
+        # save_figure(
+                # os.path.join(result_directory, 'orientation_map_zb.tex'),
+                # TikzImage('orientation_map_zb.png'))
+        # save_figure(
+                # os.path.join(result_directory, 'orientation_map_color_zb.tex'),
+                # TikzImage('orientation_map_color_zb.pdf'))
 
-        save_figure(
-                os.path.join(result_directory, 'orientation_map_wz.tex'),
-                TikzImage('orientation_map_wz.png'))
-        save_figure(
-                os.path.join(result_directory, 'orientation_map_color_wz.tex'),
-                TikzImage('orientation_map_color_wz.pdf'))
+        # save_figure(
+                # os.path.join(result_directory, 'orientation_map_wz.tex'),
+                # TikzImage('orientation_map_wz.png'))
+        # save_figure(
+                # os.path.join(result_directory, 'orientation_map_color_wz.tex'),
+                # TikzImage('orientation_map_color_wz.pdf'))
 
 
         # dp = hs.load('D:/Dokumenter/MTNANO/Prosjektoppgave/SPED_data_GaAs_NW/gen/Julie_180510_SCN45_FIB_a_three_phase_single_area.hdf5'
